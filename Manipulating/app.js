fdedc8f38cc27c9923322110a2c3b67361af91d4
orhansecic <orhan.secic@stu.ssst.edu.ba>
2021-01-24 02:10:38 +0100

    Finished Section 24: Video 251.

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -1,15 +1,3 @@
-const allLinks = document.querySelectorAll('a');
-
-// for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
-// }
-
-
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
 /*
 var foo = document.getElementsByTagName("img");
 
@@ -20,10 +8,32 @@ for(let i = 0; i<foo.length; i++){
     foo[i].src = "https://images.ctfassets.net/cnu0m8re1exe/70JhB0XIHJmprEsQmEIMx3/eee66f1505b8b1d53ff0f3b18b305a5e/shutterstock_1099958171.jpg?w=650&h=433&fit=fill";
   };
 };
-*/
+var allImg = document.querySelectorAll("img");
 
+for(let image of allImg){
+  if(image.getAttribute("class") === "square"){
+    image.setAttribute("src", "https://st3.depositphotos.com/2591003/17487/i/600/depositphotos_174871074-stock-photo-berlin-germany-march-2017-master.jpg");
+  };
+};
+*/
+/*
 var items = document.querySelectorAll("p a");
 
 for(let item of items){
   console.log(item.href);
 };
+
+console.log(document.querySelector("p"));
+*/
+/*
+var numbers = [1, 2, 3, 4, 5  ];
+var large = [9, 8, 7, 6, 5];
+
+if(numbers.length != large.length){
+  throw "They are not the same length!"
+}else{
+for ( let i = 0; i<numbers.length; i++){
+  console.log(numbers[i] + large[i]);
+};
+};
+*/
