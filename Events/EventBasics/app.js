fd55564c241a828c5dc0c9b5fef84571dd042ac5
orhansecic <orhan.secic@stu.ssst.edu.ba>
2021-01-31 13:46:03 +0100

    Finished Section 25: Video 259.

--- Events/EventBasics/app.js
+++ Events/EventBasics/app.js
@@ -18,9 +18,34 @@ function background(){
 
 var buttons = document.querySelectorAll("button");
 
+/*
 for(let i = 0; i<buttons.length; i++){
   buttons[i].onclick = function(){
     console.log("It works");
     console.log("I hope so");
   }
 }
+*/
+var title = document.querySelector("h1");
+title.addEventListener("click", function(){
+  document.querySelector("body").style.backgroundColor = "red";
+})
+title.addEventListener("dblclick", function(){
+  body.style.backgroundColor = "white";
+})
+
+function appearIMG(){
+  document.querySelector("img").style.display = "initial";
+}
+
+function disappearIMG(){
+  document.querySelector("img").style.display = "none";
+}
+
+function twist(){
+  console.log("TWIST!");
+};
+
+function shout(){
+  console.log("SHOUT!");
+};
--- Events/RandomColors/app.js
+++ Events/RandomColors/app.js
@@ -1,16 +1,52 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
+/*
+var r = 255;
+var g = 255;
+var b = 0;
 
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
 
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+document.querySelector("body").style.backgroundColor = `rgb( ${r}, ${g}, ${b})`;
+*/
+/*
+for(let i=0; i<10; i++){
+  console.log(Math.ceil(Math.random() * 255));
+};
+*/
+
+function randomColor(){
+  var r = Math.ceil(Math.random() * 255);
+  var g = Math.ceil(Math.random() * 255);
+  var b = Math.ceil(Math.random() * 255);
+
+  var body = document.querySelector("body");
+  body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+  displayColor();
+
+  function displayColor(){
+      var colorDisplay = document.querySelector("h2");
+      colorDisplay.innerText = `rgb(${r}, ${g}, ${b})`;
+
+      if(r + g + b < 150){
+        colorDisplay.style.color = "white";
+      }else{
+        colorDisplay.style.color = "black";
+      }
+}
+};
+
+
+
+var dugme = document.querySelector("button");
+dugme.addEventListener("click", randomColor);
+dugme.addEventListener("click", randomColor.displayColor);
+
+
+var number = 0;
+
+function counter(){
+  var broj = document.querySelector("p");
+  broj.innerText = `Counter ${++number}`;
 }
 
+var counterBTN = document.querySelector("#counter-btn");
+counterBTN.addEventListener("click", counter);
