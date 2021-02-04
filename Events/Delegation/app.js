d5b5e7570c53163354d99546eb214d37f4b4fccc
orhansecic <orhan.secic@stu.ssst.edu.ba>
2021-02-04 11:19:39 +0100

    Finished Section 25.

--- Events/Delegation/app.js
+++ Events/Delegation/app.js
@@ -19,7 +19,6 @@ const addTweet = (username, tweet) => {
     tweetsContainer.append(newTweet);
 }
 
-tweetsContainer.addEventListener('click', function (e) {
-    e.target.nodeName === 'LI' && e.target.remove();
+tweetsContainer.addEventListener("click", function(e){
+  e.target.remove();
 })
-
